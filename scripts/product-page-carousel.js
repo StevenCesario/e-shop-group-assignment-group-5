2ba3f4d6d8291a9211d796053c8f66e659316f91
StevenCesario <steven.lennartsson@gmail.com>
2025-12-28 15:52:38 +0100

    Adding the click listeners + ändra namnen i korten så att vi ser när saker scrollas + fixade en bug i <head> där defer va med i src=

--- scripts/product-page-carousel.js
+++ scripts/product-page-carousel.js
@@ -13,3 +13,16 @@
     // Card width + Gap (16px)
     return card.offsetWidth + gap; 
   };
+
+  // 3. Lägg till click listeners, igen med arrow functions (callback specifikt i detta fall)
+  // 'left' i denna context (med scrollBy) betyder "Horizontal Offset."
+  // För att scrolla i motsatta riktiningen lägger vi bara till ett minus för att signalera "motsatt"
+  nextBtn.addEventListener('click', () => {
+    // Scrolla till höger
+    track.scrollBy({ left: scrollAmount(), behavior: 'smooth'} )
+  });
+
+  prevBtn.addEventListener('click', () => {
+    // Scrolla till vänster
+    track.scrollBy({ left: -scrollAmount(), behavior: 'smooth'})
+  });
