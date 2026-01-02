927e14fe0f9b79f812cfa27ff34a7f4b883dfdfc
Augustlyden <august.lyden@outlook.com>
2026-01-02 11:52:15 +0100

    Upgraded up button with javascript

--- scripts/general.js
+++ scripts/general.js
@@ -6,3 +6,20 @@ function myFunction() {
     x.style.display = "block";    
     }
 }
+
+let button = document.getElementById("up-btn");
+
+window.onscroll = function() {scrollFunction()}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
